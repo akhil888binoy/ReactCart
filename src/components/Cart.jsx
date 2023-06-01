d97cdc52062b7fece768a26f359909f92ed698a4
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-06-01 23:10:59 +0530

    add item in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import {AiFillDelete} from "react-icons/ai"
 
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+
 function Cart() {
   return (
     <div className='cart'>
         <main>
-            <CartItem></CartItem>
+            <CartItem 
+                imgSrc={img1}
+                name={"Mac book"}
+                price ={1234}
+                qty ={1}
+                
+                id='asdffds'
+            
+            ></CartItem>
         </main>
         <aside>
             <h2>Subtotal:${2000}</h2>
@@ -17,7 +28,16 @@ function Cart() {
     </div>
   )
 }
-const CartItem=({imgSrc, name, price, qty, decrement, increment, deleteHandler,id})=>(
+const CartItem=({
+    imgSrc, 
+    name, 
+    price, 
+    qty, 
+    decrement, 
+    increment, 
+    deleteHandler,
+    id
+})=>(
     <div className="cartItem">
         <img src={imgSrc} alt="Item" />
         <article>
