0689bc7451265128236faedd604ec7967503d8aa
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-06-01 19:52:29 +0530

    add products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
 
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+const img2 =
+  "https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-HaleBlack-ReLive-Knits-Jogger-FrontRightLogo-Comfortable-Shoes_1024x.jpg?v=1662876260";
+
+
 const Home=()=>{
     const productList=[{
         name:"macBook",
         price: 12000, 
-        imgSrc: "asd",
+        imgSrc: img1,
         id:"asasdfgh"
     },
     {
         name:"samsung",
         price: " 130000",
-        imgSrc: "asd",
+        imgSrc: img2,
         id:"asasddddfgh"
     }
 
 ]
+const addToCartHandler=(options)=>{
+    console.log(options)
 
+}
 return (
     <div className="home">
         {
-            productList.map(i=>{
-                <ProductCard key={i.id} imgSrc={i.name} ></ProductCard>
-            })
+            productList.map((i)=>(
+                <ProductCard 
+                key={i.id} 
+                imgSrc={i.imgSrc} 
+                name={i.name}
+                price={i.price} 
+                id={i.id}
+                handler={addToCartHandler}></ProductCard>
+            ))
         }
     </div>
 )
@@ -34,7 +49,8 @@ const ProductCard=({name,id,price,handler,imgSrc})=>(
         <img src={imgSrc} alt={name} />
         <p>{name}</p>
         <h4>${price}</h4>
-        <button onClick={()=>handler()}>Add to cart</button>
+        <button onClick={()=>handler({name,price,id,quantity:1, imgSrc
+        })}>Add to cart</button>
     
     </div>
 )
